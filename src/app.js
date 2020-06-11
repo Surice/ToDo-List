03748515060abe2db7a07704bbd354f6a199f6cc
Sebastian Ulrich <sesi.ulrich@gmail.com>
2020-06-11 18:11:19 +0200

    Simple start page added

--- src/app.js
+++ src/app.js
@@ -2,23 +2,35 @@ const electron = require('electron');
 
 const { app, BrowserWindow} = require('electron');
 
+
+
 app.on('ready', function(){
-    //erstellt ein neues fenster
-    let win = new BrowserWindow({
-      width: 1200,
-      height: 600,
-      title: "ToDo-List",
-      center: true,
-      frame: false,
-      transparent: true,
-      autoHideMenuBar: true,
-      icon: `${__dirname}/icon.png`,
+  let startWin = new BrowserWindow({width:500, height:120, transparent:true, frame:false, center:true, webPreferences:{nodeIntegration:true}});
+  startWin.loadFile('./start.html');
+
+  //erstellt ein neues fenster
+  let win = new BrowserWindow({
+    width: 1200,
+    height: 600,
+    show: false,
+    title: "ToDo-List",
+    center: true,
+    frame: false,
+    transparent: true,
+    autoHideMenuBar: true,
+    icon: `./icon.png`,
       
-      webPreferences: {
-        nodeIntegration: true
-      }
-    });
-  
-   //läd in das fenster die gebaute html
-    win.loadFile('main.html');
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  //läd in das fenster die gebaute html
+  win.loadFile('./main.html');
+
+  win.on('ready-to-show', ()=>{
+    console.log("ready!");
+    startWin.close();
+    win.show();
+  });
+
 });
