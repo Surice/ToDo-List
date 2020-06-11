30c9c8a573d652d9c7ccfe33c1ff7d8d620da55f
Sebastian Ulrich <sesi.ulrich@gmail.com>
2020-06-11 16:00:23 +0200

    new electron-app structure

--- src/app.js
+++ src/app.js
@@ -1,33 +1,24 @@
 const electron = require('electron');
 
-const { app, BrowserWindow, globalShortcut, Notification} = require('electron');
+const { app, BrowserWindow} = require('electron');
 
-function createWindow () {
-  // Erstelle das Browser-Fenster.
-  let win = new BrowserWindow({
-    width: 1200,
-    height: 600,
-    title: "ToDo-List",
-    fullscreen: false,
-    center: true,
-    frame: false,
-    transparent: true,
-    autoHideMenuBar: true,
-    darkTheme: true,
-    icon: `${__dirname}/icon.png`,
-    
-    
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-
-  // und lade die index.html der App.
-  win.loadFile('main.html');
-
-  globalShortcut.register('Esc', () => {
-    win.close();
-  });
-}
-
-app.whenReady().then(createWindow);
+app.on('ready', function(){
+    //erstellt ein neues fenster
+    let win = new BrowserWindow({
+      width: 1200,
+      height: 600,
+      title: "ToDo-List",
+      center: true,
+      frame: false,
+      transparent: true,
+      autoHideMenuBar: true,
+      icon: `${__dirname}/icon.png`,
+      
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+  
+   //läd in das fenster die gebaute html
+    win.loadFile('main.html');
+});
